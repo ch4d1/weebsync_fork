dd960acc6f524cc3900c3a09aa21d33ad747659a
ch4d1 <git@chadi.de>
2025-08-24 17:24:32 +0200

    feat: migrate to ESLint 9 with flat config format
    - Replace deprecated .eslintrc with modern eslint.config.js
    - Update configuration for TypeScript and Vue support
    - Improve code quality rules and Prettier integration

---
+++ eslint.config.js
@@ -0,0 +1,61 @@
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import vuePlugin from "eslint-plugin-vue";
+import vueParser from "vue-eslint-parser";
+import prettierPlugin from "eslint-plugin-prettier";
+
+export default [
+  {
+    files: ["**/*.js", "**/*.ts"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      parser: tsParser,
+      globals: {
+        NodeJS: true,
+        process: true,
+        console: true,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": ["error", {}],
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+    },
+  },
+  {
+    files: ["**/*.vue"],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+      },
+      globals: {
+        NodeJS: true,
+        process: true,
+        console: true,
+      },
+    },
+    plugins: {
+      vue: vuePlugin,
+      "@typescript-eslint": tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": ["error", {}],
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+      "vue/multi-word-component-names": "off",
+    },
+  },
+];
